de03a64cde3b49f5fff24db71faa5fa56f5bb32e
andradebru <brunansandrade@gmail.com>
2022-09-13 11:12:11 -0300

    add layouts

---
+++ src/views/ListaTarefas.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container">
+    <h1>{{ msg }}</h1>
+    <ul class="collection">
+      <li v-for="t in tasks" :key="t.id" class="collection-item">
+        <h5>
+          {{ t.title }}
+          <span class="task-badge right-align">{{ t.project }}</span>
+        </h5>
+        <!-- {{ t }} -->
+        <v-btn class="blue ligthen-1" @click="recebiEditar">Editar</v-btn>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+    tasks: Array,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.task-badge {
+  background-color: #929292;
+  color: white;
+  padding: 6px;
+  border-radius: 6px;
+  font-size: 0.8rem;
+  vertical-align: middle;
+}
+</style>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-container>
+    <v-col>
+      <h1>LOGIN</h1>
+
+      <v-text-field
+        label="Usuário"
+        :rules="rules"
+        hide-details="auto"
+      ></v-text-field>
+      <v-text-field label="Senha"></v-text-field>
+
+      <v-btn class="blue ligthen-1">Login</v-btn>
+      <!-- @click="vaiParaLogin" -->
+    </v-col>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    rules: [
+      (value) => !!value || "É obrigatório entrar com um usuário.",
+      (value) => (value && value.length >= 3) || "Min 3 characters",
+    ],
+  }),
+};
+</script>
