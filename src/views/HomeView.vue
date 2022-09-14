947dee7406e71c6a2b11c0940d895f43191a24c4
andradebru <brunansandrade@gmail.com>
2022-09-14 10:01:02 -0300

    add api mock

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,83 +1,57 @@
 <template>
-  <v-card :loading="loading" class="mx-auto my-12" max-width="374">
-    <template slot="progress">
-      <v-progress-linear
-        color="deep-purple"
-        height="10"
-        indeterminate
-      ></v-progress-linear>
-    </template>
-
-    <v-img
-      height="250"
-      src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
-    ></v-img>
-
-    <v-card-title>Cafe Badilico</v-card-title>
-
+  <v-card class="mx-auto" max-width="344">
     <v-card-text>
-      <v-row align="center" class="mx-0">
-        <v-rating
-          :value="4.5"
-          color="amber"
-          dense
-          half-increments
-          readonly
-          size="14"
-        ></v-rating>
-
-        <div class="grey--text ms-4">4.5 (413)</div>
-      </v-row>
-
-      <div class="my-4 text-subtitle-1">$ • Italian, Cafe</div>
-
-      <div>
-        Small plates, salads & sandwiches - an intimate setting with 12 indoor
-        seats plus patio seating.
+      <div>Word of the Day</div>
+      <p class="text-h4 text--primary">el·ee·mos·y·nar·y</p>
+      <p>adjective</p>
+      <div class="text--primary">
+        relating to or dependent on charity; charitable.<br />
+        "an eleemosynary educational institution."
       </div>
     </v-card-text>
-
-    <v-divider class="mx-4"></v-divider>
-
-    <v-card-title>Tonight's availability</v-card-title>
-
-    <v-card-text>
-      <v-chip-group
-        v-model="selection"
-        active-class="deep-purple accent-4 white--text"
-        column
-      >
-        <v-chip>5:30PM</v-chip>
-
-        <v-chip>7:30PM</v-chip>
-
-        <v-chip>8:00PM</v-chip>
-
-        <v-chip>9:00PM</v-chip>
-      </v-chip-group>
-    </v-card-text>
-
     <v-card-actions>
-      <v-btn color="deep-purple lighten-2" text @click="reserve">
-        Reserve
+      <v-btn text color="teal accent-4" @click="reveal = true">
+        Learn More
       </v-btn>
     </v-card-actions>
+
+    <v-expand-transition>
+      <v-card
+        v-if="reveal"
+        class="transition-fast-in-fast-out v-card--reveal"
+        style="height: 100%"
+      >
+        <v-card-text class="pb-0">
+          <p class="text-h4 text--primary">Origin</p>
+          <p>
+            late 16th century (as a noun denoting a place where alms were
+            distributed): from medieval Latin eleemosynarius, from late Latin
+            eleemosyna ‘alms’, from Greek eleēmosunē ‘compassion’
+          </p>
+        </v-card-text>
+        <v-card-actions class="pt-0">
+          <v-btn text color="teal accent-4" @click="reveal = false">
+            Close
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-expand-transition>
   </v-card>
 </template>
 
 <script>
 export default {
   data: () => ({
-    loading: false,
-    selection: 1,
+    reveal: false,
   }),
-
-  methods: {
-    reserve() {
-      this.loading = true;
-
-      setTimeout(() => (this.loading = false), 2000);
-    },
-  },
 };
 </script>
+
+<style>
+.v-card--reveal {
+  bottom: 0;
+  opacity: 1 !important;
+  position: absolute;
+  width: 100%;
+}
+</style>
