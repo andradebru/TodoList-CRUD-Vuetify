13d04ac25ff7f34bb165f80fc97ed90f8861ebe6
andradebru <brunansandrade@gmail.com>
2022-09-16 14:17:36 -0300

    add initial page

--- src/main.js
+++ src/main.js
@@ -4,6 +4,21 @@ import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
 
+/* import the fontawesome core */
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+/* import specific icons */
+import { faCalendarCheck } from "@fortawesome/free-solid-svg-icons";
+
+/* add icons to the library */
+library.add(faCalendarCheck);
+
+/* add font awesome icon component */
+Vue.component("font-awesome-icon", FontAwesomeIcon);
+
 Vue.config.productionTip = false;
 
 new Vue({
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,72 +1,11 @@
 <template>
   <v-container>
-    <h1>ToDo List</h1>
-    <div>
-      <v-col>
-        <h2>LOGIN</h2>
-
-        <v-text-field
-          label="Usuário"
-          :rules="rules"
-          hide-details="auto"
-        ></v-text-field>
-        <v-text-field label="Senha"></v-text-field>
-
-        <v-btn class="blue ligthen-1">Login</v-btn>
-        <!-- @click="vaiParaLogin" -->
+    <v-row class="text-center">
+      <v-col cols="12">
+        <font-awesome-icon class="text-h2" icon="fa-solid fa-calendar-check" />
+        <h1>Bem vindo ao Vue To-Do List</h1>
+        <v-btn>INICIAR</v-btn>
       </v-col>
-      <!-- <TarefaCard /> -->
-    </div>
+    </v-row>
   </v-container>
 </template>
-
-<script>
-// import TarefaCard from "../components/TarefaCard.vue";
-
-export default {
-  components: {
-    // TarefaCard,
-  },
-  // methods: {
-  //   listarTarefas() {
-  //     TasksApi.getTasks((data) => {
-  //       this.listaDeTarefa = data;
-  //     });
-  //   },
-  //   mostrarCadastro() {
-  //     this.form.btn = "Adicionar";
-  //     this.exibir.form = true;
-  //     this.exibir.lista = false;
-  //   },
-  //   recebiSalvar(novaTarefa) {
-  //     TasksApi.createTask(novaTarefa, () => {
-  //       this.listarTarefas();
-  //       this.exibir.form = false;
-  //       this.exibir.lista = true;
-  //     });
-  //   },
-  //   recebiAlterar(tarefa) {
-  //     TasksApi.updateTask(tarefa, () => {
-  //       this.listarTarefas();
-  //       this.exibir.form = false;
-  //       this.exibir.lista = true;
-  //     });
-  //   },
-  //   recebiEditar(tarefaId) {
-  //     this.form.btn = "Alterar";
-  //     TasksApi.getTask(tarefaId, (task) => {
-  //       this.form.id = task.id;
-  //       this.form.title = task.title;
-  //       this.form.project = task.project;
-  //       this.exibir.form = true;
-  //       this.exibir.lista = false;
-  //     });
-  //   },
-  // },
-  // created() {
-  //   this.listarTarefas();
-  // },
-};
-</script>
-
-<style></style>
